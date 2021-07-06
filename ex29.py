83fef7b553ddb57cf4b62026139d2d5ff2f372fd
michodgs24 <57402787+michodgs24@users.noreply.github.com>
2021-07-06 23:00:55 +0100

    Create if statement examples

---
+++ ex29.py
@@ -0,0 +1,42 @@
+# Create three variables and attach a value to each
+people = 20
+cats = 30
+dogs = 15
+
+# if statement, checks whether there are less people than cats
+if people < cats:
+    # prints result
+    print("Too many cats! The world is doomed!")
+
+# if statement, checks whether there are more cats than people
+if people > cats:
+    # prints result
+    print("Not many Cats! The world is saved!")
+
+# if statement, check if more dogs than people
+if people < dogs:
+    # print result
+    print("The world is drooled on!")
+
+# if statement, check if more people than dogs
+if people > dogs:
+    # print result
+    print("The world is dry!")
+
+# add an increment of 5 to dog value
+dogs += 5
+
+# if statement, check whether people are greater than or equal to dogs
+if people >= dogs:
+    # print result
+    print("People are greater than or equal to dogs.")
+
+# if statement, check whether people are less than or equal to dogs
+if people <= dogs:
+    # print result
+    print("People are less than or equal to dogs.")
+
+# if equal statement, checks if people are dogs
+if people == dogs:
+    # print result
+    print("People are dogs")

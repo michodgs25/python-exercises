7519ec799921fa94912a5186ad8da6f8ff420613
michodgs24 <57402787+michodgs24@users.noreply.github.com>
2021-07-27 17:09:01 +0100

    Create keyword code examples

--- exercises/keywords_one.py
+++ exercises/keywords_one.py
@@ -1,37 +1,32 @@
-#   Keyword     |         Description           |          Example
-# ----------------------------------------------------------------------------
+from x import y
 
-#     and       |         Logical and           |    True and False == False
-# ----------------------------------------------------------------------------
-#     as        |  Part of with-as statement    |   with X as Y: = pass
-# ----------------------------------------------------------------------------
-#    assert     | Ensure that something is True |   Assert False, "Error!"
-# ----------------------------------------------------------------------------
-#    break      |       Stop this loop now      |   while True: break
-# ----------------------------------------------------------------------------
-#   class       |         define a class        |  class person(object)
-# ----------------------------------------------------------------------------
-#   continue    | Don't process more of loop,   | while True: continue
-#                   do it again buddy
-# ----------------------------------------------------------------------------
-#    def        | define a function             | def x(): pass
-# ----------------------------------------------------------------------------
-#    del        | Delete from dictionary        | del x[y]
-# ----------------------------------------------------------------------------
-#   elif        | else if condition             | if: x; elif: y; else: j
-# ----------------------------------------------------------------------------
-#   else        | Else condition                | if: x; elif: y; else: j
-# ----------------------------------------------------------------------------
-#   except      |  If an exception happens,     | except ValueError as e: print(e)
-#                         do this
-# ----------------------------------------------------------------------------
-#    exec       |  Run a string as Python       | exec 'print("hello")'
-# ----------------------------------------------------------------------------
-#   finally     | Exceptions or not, finally,   | finally: pass
-#                 do this no matter what
-# ----------------------------------------------------------------------------
-#    for        | loop over a collection things | for x in y: pass
-# ----------------------------------------------------------------------------
-#   from        | Import specific parts of a    | from x import y
-#                          module.
-# ----------------------------------------------------------------------------
+True and False == False
+
+with X as Y: = pass
+
+assert False, "Error!"
+
+while True: break
+
+class person(object):
+    print(object)
+
+while True:
+    continue
+
+def a():
+    pass
+
+del x[y]
+
+if: x; elif: y; else: j
+
+if: x; elif: y; else: j
+
+except ValueError as e: print(e)
+
+exec 'print("hello")'
+
+finally: pass
+
+for x in y: pass

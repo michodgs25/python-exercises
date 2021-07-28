3ef8adac2027705f8aad84e3190f6af2355778f5
michodgs24 <57402787+michodgs24@users.noreply.github.com>
2021-07-28 17:29:04 +0100

    Explain the purpose of using lists in programming

--- exercises/modify-lists.py
+++ exercises/modify-lists.py
@@ -54,3 +54,57 @@ print(stuff[-1])
 print(stuff.pop())
 print(' '.join(stuff))
 print('#'.join(stuff[3:5]))
+
+
+
+
+#                          WHAT LISTS CAN DO
+# ----------------------------------------------------------------------------
+# Let's say you want to create a computer game based on go fish.
+# To do this you would need to have some way of taking the concept of a "deck of cards",
+# and putting it into your Python program.
+
+# You then have to write Python code that knows how to work this imaginary version of a deck of cards,
+# so that a person playing your game thinks that it is real, even if it isn't.
+# What you need is a "deck of cards" structure, and programmers call this kind of thing a data structure.
+
+# A data structure is just a formal way to structure some data.
+# Even if some data structures can get very complex, all they are is just a way to store facts inside a program.
+# So you can access them in different ways. They structure data.
+
+# Lists are one of the most common data structures programmers use.
+# A list is an ordered list of things you want to store and access randomly or linearly by an index.
+
+# Deck of cards list example:
+
+# 1. You have a bunch of cards with values
+
+# 2. Those cards are in a stack, list, or list from the top card to the bottom card.
+
+# 3. You can take cards off the top, the bottom or the middle at random.
+
+# 4. If you want to find a specific card, you have to grab the deck and go through it one at a time.
+
+print("An ordered list, the deck of cards is in order, with a first and a last.")
+print("of things you want to store, cards can be things I want to store.")
+print("And access randomly. Any card from the deck can be accessed at random.")
+print("Or accessed linearly, start at the beginning and go through the deck in order.")
+print("By an index, access the card by its index position in the deck.")
+
+print("""
+That is all a list does.
+Every concept in programming usually has some relationship to the real world, at least the usedful ones.
+""")
+
+
+# WHEN TO USE LISTS
+# --------------------
+
+print("""
+You can use a list whenever you have something that matches the list data structure's useful features:
+     1. If you need to maintain order. This is listed order not sorted order, Lists do not sort for you.
+
+     2. If you need to access the contents randomly by a number, remember this uses cardinal numbers: start at 0.
+
+     3. If you need to go through the contents linearly(first to last). Remember that's what for-loops are for.
+ """)

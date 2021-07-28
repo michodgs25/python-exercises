e42c9740eefa657e57a3fef017a295b0b372ae7c
michodgs24 <57402787+michodgs24@users.noreply.github.com>
2021-07-28 10:51:37 +0100

    Create problem-solve folder and arrays.py file

---
+++ problem-solve/arrays.py
@@ -0,0 +1,5 @@
+# Arrays are one of the fundamental data structures in computer science.
+# An Array is represented as a fixed sized, contiguous block of memory,
+# with 0(1) time to store and access an element.
+
+# 

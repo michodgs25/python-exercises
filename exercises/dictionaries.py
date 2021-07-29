12b45d5b5e0670d72c2b2c7dab31cb74e4918ac0
michodgs24 <57402787+michodgs24@users.noreply.github.com>
2021-07-29 18:54:23 +0100

    Create for-loop to iterate through mapped keys, print specified values

--- exercises/dictionaries.py
+++ exercises/dictionaries.py
@@ -1,83 +1,83 @@
 # Dictionaries
 
-print("""
+print(
+    """
 A dictionary or `dict` in Python, is a way to store data just like a list.
 But instead of using only numbers to get the data, you can use almost anything.
 This let's you treat a dict like it's a database for storing and organising data.
-""")
+"""
+)
 
 print("Dictionary Example:")
 
 # Create a mapping of state to abbreviation
 states = {
-    'Oregon': 'OR',
-    'Florida': 'FL',
-    'California': 'CA',
-    'New York': 'NY',
-    'Michigan': 'MI',
+    "Oregon": "OR",
+    "Florida": "FL",
+    "California": "CA",
+    "New York": "NY",
+    "Michigan": "MI",
 }
 
 # Create set of states with cities in them
-cities = {
-    'CA': 'San Fransisco',
-    'MI': 'Detroit',
-    'FL': 'Jackonville'
-}
+cities = {"CA": "San Fransisco", "MI": "Detroit", "FL": "Jackonville"}
 
 # Add some more cities
-cities['NY'] = 'New York'
-cities['OR'] = 'Portland'
+cities["NY"] = "New York"
+cities["OR"] = "Portland"
 
 # print out some cities
-print('-' * 10)
-print("NY State has: ", cities['NY'])
-print("OR State has: ", cities['OR'])
+print("-" * 10)
+print("NY State has: ", cities["NY"])
+print("OR State has: ", cities["OR"])
 
 # Print some states
-print('-' * 10)
-print("Michigan's abbreviation is: ", states['Michigan'])
-print("Florida's abbreviation is: ", states['Florida'])
+print("-" * 10)
+print("Michigan's abbreviation is: ", states["Michigan"])
+print("Florida's abbreviation is: ", states["Florida"])
 
 # do it by using the states then cities dict
-print('-' * 10)
-print("Michigan has: ", cities[states['Michigan']])
-print("Florida has: ", cities[states['Florida']])
+print("-" * 10)
+print("Michigan has: ", cities[states["Michigan"]])
+print("Florida has: ", cities[states["Florida"]])
 
 # print every state abbreviation
-print('-' * 10)
+print("-" * 10)
 for state, abbrev in list(states.items()):
     print(f"{state} state is abbreviated {abbrev}")
 
 # print every city in state
-print('-' * 10)
+print("-" * 10)
 for abbrev, city in list(cities.items()):
     print(f"{abbrev} has the city {city}")
 
 # now do both at the same time
-print('-' * 10)
+print("-" * 10)
 for state, abbrev in list(states.items()):
     print(f"{state} state is abbreviated {abbrev}.")
     print(f"and has city {cities[abbrev]}")
 
-print('-' * 10)
+print("-" * 10)
 # safely get the abbreviation by state that might not be there
-state = states.get('Texas')
+state = states.get("Texas")
 
 if not state:
     print("Sorry, no Texas.")
 
 # get a city with a default value
-city = cities.get('TX', 'Does not exist')
+city = cities.get("TX", "Does not exist")
 print(f"The city for the state 'TX' is: {city}")
 
 
-print("""
+print(
+    """
 What Dictionaries can do
 
 A dictionary is another example of a data structure, and like a list,
 it is one of the most commonly used data structures in programming.
 A dict is used to map or associate things you want to store to keys you need to get.
- """)
+ """
+)
 
 
 # England cities and their football teams
@@ -85,25 +85,41 @@ A dict is used to map or associate things you want to store to keys you need to 
 # map city examples
 
 england_cities = {
-        'Birmingham': 'BI',
-        'Aberdeen': 'AN',
-        'leeds': 'LD',
-        'Liverpool': 'LP',
-        'Manchester': 'MA',
-        'London': 'LCY'
+    "Birmingham": "BI",
+    "Aberdeen": "AN",
+    "leeds": "LD",
+    "Liverpool": "LP",
+    "Manchester": "MA",
+    "London": "LCY",
 }
 
-# Map football teams
+# Map city abbreviations and their football teams
 football_teams = {
-         'BI': 'Aston Villa, Birmingham City',
-         'AN': 'Aberdeen',
-         'LP': 'Liverpool, Everton, Tramere Rovers',
-         'MA': 'Manchester United, Manchester City, Salford',
-         'LD': 'Leeds United',
-         'LCY': 'Chelsea, Arsenal, Spurs, Crystal Palace, Fulham',
+    "BI": "Aston Villa, Birmingham City",
+    "AN": "Aberdeen",
+    "LP": "Liverpool, Everton, Tramere Rovers",
+    "MA": "Manchester United, Manchester City, Salford",
+    "LD": "Leeds United",
+    "LCY": "Chelsea, Arsenal, Spurs, Crystal Palace, Fulham",
 }
 
 # print out some teams
-print('-' * 10)
-print("LCY city has: ", football_teams['LCY'])
-print("MA city has: ", football_teams['MA'])
+print("-" * 10)
+print("LCY city has: ", football_teams["LCY"])
+print("MA city has: ", football_teams["MA"])
+
+# print out some cities
+print("-" * 20)
+print("Manchester abbreviation is: ", england_cities["Manchester"])
+print("Liverpool abbreviation is: ", england_cities["Liverpool"])
+
+
+# print out every city abbreviations
+print("-" * 30)
+for city, abbrev in list(england_cities.items()):
+    print(f"{city} is abbreviated: {abbrev}")
+
+# print out football teams city abbreviations
+print("-" * 40)
+for team, city in list(football_teams.items()):
+    print(f"{city} teams are: {team}")

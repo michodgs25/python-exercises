efd31497219415d10bbdf2569fa9cac01dd47c46
michodgs24 <57402787+michodgs24@users.noreply.github.com>
2021-08-02 16:02:59 +0100

    Create moreStuff() class, return apple and tangerine objects

--- exercises/modules_classes_objects.py
+++ exercises/modules_classes_objects.py
@@ -7,6 +7,7 @@ print(mystuff.tangerine)
 
 print("""
 Python is called an 'object oriented programming language'.
+
 This means there is a construct within python called a class,
 which let's you structure your software in a particular way.
 Using classes can add consistency to your programs so that they be used in a cleaner way.
@@ -28,7 +29,7 @@ In the case of the module, the key is an identifier, and the syntax is .key
 
 print("""
 Classes are like modules
--------------------------
+------------------------------
 
 You can think about a module as a specialized dictionary that can store python code,
 so you can access it with the . operator.
@@ -36,4 +37,19 @@ Python also has another construct that serves a similar purpose called a class.
 
 A class is a way to take a grouping of functions& data and place them inside a container,
 so you can access them with the . (dot) operator.
+""")
+
+
+class moreStuff(object):
+
+    def __init__(self):
+        self.tangerine = "And now a thousand years between"
+
+    def apples(self):
+        print("I AM CLASSY APPLES!")
+
+
+print("""
+A class looks more complicated in comparison to modules, due to there be more going on.
+Class can be viewed as like a mini-module with moreStuff having an apple() function in it.
 """)

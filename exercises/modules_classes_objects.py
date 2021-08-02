679f522d78ed597f40b0a12813db701e72391098
michodgs24 <57402787+michodgs24@users.noreply.github.com>
2021-08-02 17:33:39 +0100

    Create three example of getting things from things

--- exercises/modules_classes_objects.py
+++ exercises/modules_classes_objects.py
@@ -40,16 +40,90 @@ so you can access them with the . (dot) operator.
 """)
 
 
-class moreStuff(object):
+class MyStuff(object):
 
     def __init__(self):
         self.tangerine = "And now a thousand years between"
 
-    def apples(self):
+    def apple(self):
         print("I AM CLASSY APPLES!")
 
 
 print("""
 A class looks more complicated in comparison to modules, due to there be more going on.
-Class can be viewed as like a mini-module with moreStuff having an apple() function in it.
+
+Class can be viewed as like a mini-module with myStuff having an apple() function in it.
+Also the use of __init__() function and use of self.tangerine for setting the tangerine instance variable.
+
+Classes are used more often instead of modules:
+                you can the myStuff class and use it to craft many of them, millions more if you like,
+                and each one won't interfere with each other.
+                When you import a module there is only one for the entire program.
 """)
+
+
+print("""
+Objects are like import
+---------------------------
+
+If a class is like a mini-module, then there has to be a concept similar to import but for classes.
+
+That concept is called instantiate, which is just another way of saying create.
+When you instantiate/create a class, what you get is an object.
+
+You instantiate(create) a class by calling the class like it's a function:
+""")
+
+thing = MyStuff()
+thing.apple()
+print(thing.tangerine)
+
+print("""
+The first line is the instantiate operation, which is similar to calling a function.
+However Python, coordinates a sequence of events behind the scenes:
+
+1. Python looks for MyStuff() and sees that it is a class that you've defined.
+
+2. Python crafts an empty object with all functions you've specified in the class using def.
+
+3. Python then looks to see if you made a magic __init__function,
+and if you have it calls that function to initialize your newly created object.
+
+4. In the myStuff function __init__ you then get this extra variable, self,
+which is that empty object Python made for you, and you can set variables on it,
+just like you would with a module, dictionary, or other object.
+
+5. In this case, you set self.tangerine to a song lyric, and then you've initialized this object.
+
+6. Now Python can take this newly minted object and assign it to the thing variable for you to work with.
+
+The above is the basics of how Python does this mini import when you call a class like a function.
+This is not using the class but instead is using the class blueprint for building a copy of that thing.
+
+At this point classes and objects diverge from modules at this point:
+
+- Classes are like blueprints or definitions for creating new mini-modules.
+
+- Instantiation is how you make one of these mini-modules and import it at the same time.
+  Instatiate just means to create an object from the class.
+
+- The resulting created mini-module is called an object,
+  and you then assign it to a variable to work with it.
+""")
+
+print("""
+Getting things from things
+--------------------------------
+""")
+
+#dict style
+mystuff['apples']
+
+# module style
+mystuff.apples()
+print(mystuff.tangerine)
+
+# class style
+thing = MyStuff()
+thing.apples()
+print(thing.tangerine)

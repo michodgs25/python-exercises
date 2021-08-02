6cbf7d0e737c519c46e84bc847af1241ef450a2c
michodgs24 <57402787+michodgs24@users.noreply.github.com>
2021-08-02 15:46:51 +0100

    Print statement to the console, explain how python modules& classes work

--- exercises/modules_classes_objects.py
+++ exercises/modules_classes_objects.py
@@ -15,4 +15,25 @@ Using classes can add consistency to your programs so that they be used in a cle
 print("""
 Modules are like dictionaries
 ------------------------------
+
+We have a very common pattern in python:
+
+1. take a key=value style container
+2. get something out of it by the key's name
+
+In the case of the dictionary, the key is a string and the syntax is [key].
+
+In the case of the module, the key is an identifier, and the syntax is .key
+""")
+
+print("""
+Classes are like modules
+-------------------------
+
+You can think about a module as a specialized dictionary that can store python code,
+so you can access it with the . operator.
+Python also has another construct that serves a similar purpose called a class.
+
+A class is a way to take a grouping of functions& data and place them inside a container,
+so you can access them with the . (dot) operator.
 """)

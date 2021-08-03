3d1491d056f8dcff5cc502627f2b657f0138b4e5
michodgs24 <57402787+michodgs24@users.noreply.github.com>
2021-08-03 16:17:55 +0100

    Create object orient.py file and print python phrases to the console

---
+++ exercises/object-oriented.py
@@ -0,0 +1,25 @@
+# OBJECT ORIENTED WORDS AND PHRASES
+
+print("""
+Python words and definitions:
+
+* class - Tell Python to make a new type of thing
+
+* object - has two meanings, the most basic type of thing, and any instance of some thing.
+
+* Instance - what you get when you tell Python to create a class.
+
+* def - how you define a function inside a class
+
+* self - Inside the functions in a class, self is a variable for the instance/object being accessed.
+
+* inheritance - The concept that one class can inherit from traits from another class.
+
+* composition -  The concept that a class can be composed of other classes as parts.
+
+* attribute - A property classes have that are from composition and are usually variables.
+
+* is-a - A phrase that to say that something inherits from another.
+
+* has-a - A phrase to say that something is composed of other things or has a trait.
+""")

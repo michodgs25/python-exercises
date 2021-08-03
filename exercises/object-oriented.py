10be6694245dd0bb321ee729119ef4b25202f0dc
michodgs24 <57402787+michodgs24@users.noreply.github.com>
2021-08-03 18:13:24 +0100

    Run class functions& attribute examples

--- exercises/object-oriented.py
+++ exercises/object-oriented.py
@@ -13,7 +13,7 @@ Python words and definitions:
 
 * self - Inside the functions in a class, self is a variable for the instance/object being accessed.
 
-* inheritance - The concept that one class can inherit from traits from another class.
+* inheritance - The concept that one class can inherit traits from another class.
 
 * composition -  The concept that a class can be composed of other classes as parts.
 
@@ -23,3 +23,27 @@ Python words and definitions:
 
 * has-a - A phrase to say that something is composed of other things or has a trait.
 """)
+
+
+class X(Y) # make a class named X that is-a Y
+
+class X(object): def __init__(self, J) # class X has-a __init__ that takes self and J parameters
+
+class X(object): def M(self, J) # class X has-a function named M that takes self and J parameters
+
+foo = X() # set foo to an instance of class X
+
+foo.M(J) # from foo, get the M function, and call it with parameters self, J
+
+foo.K = Q # from foo, get the K attribute, and set it to Q
+
+
+# In each of the above, where you can see X, Y, M, J, K, Q and foo
+# I can treat them as blank spots:
+
+# 1. make a class named ??? that is-a Y
+# 2. class ??? has-a __init__ that takes self and ??? as parameters
+# 3. class ??? has-a function named ??? that takes self and ??? as parameters
+# 4. set ??? to an instance of class
+# 5. from ??? get the ??? function& call it with self=??? and parameters ???
+# 6. from ??? get the ??? attribute and set it to ???

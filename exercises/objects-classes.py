723d2557e9eeae92d4504192b4f1fb26608b0600
michodgs24 <57402787+michodgs24@users.noreply.github.com>
2021-08-05 17:25:00 +0100

    Add comment to objects and classes file

---
+++ exercises/objects-classes.py
@@ -0,0 +1,5 @@
+# IS-A, HAS-A, OBJECTS, AND CLASSES
+
+# An important concept that you have to understand is the difference between a class and an object.
+# The problem is, there is no real difference between a class and an object.
+# They are actually the same thing at different points in time.

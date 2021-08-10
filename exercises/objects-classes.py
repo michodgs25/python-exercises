51dc20badd135375570a101b2140bd228c06d30f
michodgs24 <57402787+michodgs24@users.noreply.github.com>
2021-08-10 17:36:09 +0100

    Create sevel further classes

--- exercises/objects-classes.py
+++ exercises/objects-classes.py
@@ -36,3 +36,60 @@ class Person(object):
     def __init__(self, name):
         # is-a instance of __init__ param 'name'
         self.name = name
+
+        ## person has-a pet of some kind
+        self.pet = None
+
+# Employee is-a person
+class Employee(Person):
+
+    def __init__(self, name, salary):
+        # Super has-a init
+        super(Employee, self).__init__(name)
+        # self is-a salary
+        self.salary = salary
+
+# Fish is-a object, first instance of Fish
+class Fish(object):
+    pass
+
+# Salmon has-a relationship with Fish
+class Salmon(Fish):
+    pass
+
+# Halibut has-a relationship with Fish
+class Halibut(Fish):
+    pass
+
+
+# rover is-a Dog
+rover = Dog("Rover")
+
+# satan is-a Cat
+satan = Cat("Satan")
+
+# mary is-a Person
+mary = Person("Mary")
+
+# from mary get pet attribute, set it to 'satan'
+mary.pet = satan
+
+# Set Frank to Employee, which takes two params "Frank" and 120000
+frank = Employee("Frank", 120000)
+
+# from frank get pet attribute and set it to rover
+frank.pet = rover
+
+# set flipper to class Fish()
+flipper = Fish()
+
+# set crouse to class Salmon()
+crouse = Salmon()
+
+# set harry to class Halibut()
+harry = Halibut()
+
+print("""
+In python3, you do not need to add the (object) after the name of the class,
+but the Python community believes in 'explicit is better than implicit'.
+""")
